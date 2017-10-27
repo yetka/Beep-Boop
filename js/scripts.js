70ab458425ad2bf67cf8560dce782cedd78be521
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-10-27 11:43:50 -0700

    add back-end part to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+//back-end
+var beebBoop = function (inputNumber) {
+  var numbers = "";
+  if (inputNumber % 3 === 0) {
+    return numbers.concat("I'm sorry, Dave. I'm afraid I can't do that.")
+  } else {
+    stringNumber = inputNumber.toString();
+    if ((stringNumber.includes("1")) || (stringNumber.includes("0"))) {
+        if (stringNumber.includes("0")) {
+          numbers = numbers.concat("Beep!");
+        };
+        if (stringNumber.includes("1")) {
+          numbers = numbers.concat("Boop!");
+        };
+      return numbers;
+    } else {
+      for (index = 0; index <= stringNumber; index++) {
+        numbers = numbers.concat(index + ",");
+      };
+      return numbers;
+    };
+  };
+};
+
+
+
+//front-end
+
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
